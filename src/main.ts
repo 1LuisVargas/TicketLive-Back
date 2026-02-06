4f934b63e7945b1aae81616b12a83413a5da6bba
Luis Vargas <luisvargas98@hotmail.com>
2026-02-06 13:00:38 -0500

    Trying to fix CORS block

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
-import { LoggerMiddleware } from "./middlewares/logger.middleware";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import cookieParser from "cookie-parser";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import * as express from "express";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  console.log("ENV CHECK", {
+  console.log('ENV CHECK', {
     DB_HOST: process.env.DB_HOST,
     DB_PASSWORD_TYPE: typeof process.env.DB_PASSWORD,
     MAIL_HOST: process.env.MAIL_HOST,
@@ -19,19 +19,14 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.set("trust proxy", 1);
+  app.set('trust proxy', 1);
 
-  app.use(express.json({ limit: "10mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
   const FRONTEND_URL = process.env.FRONTEND_URL || process.env.FRONT_URL!;
 
-  const allowedOrigins = [
-    // "http://localhost:3005",
-    // "http://localhost:3000",
-    "https://ticket-live-1.vercel.app",
-    FRONTEND_URL,
-  ].filter((v): v is string => typeof v === "string" && v.length > 0);
+  const allowedOrigins = [FRONTEND_URL].filter(Boolean);
 
   app.enableCors({
     origin: (origin, callback) => {
@@ -54,27 +49,27 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       transform: true,
-    })
+    }),
   );
 
   app.use(LoggerMiddleware);
 
   const swaggerConfig = new DocumentBuilder()
-    .setTitle("TicketLive API")
-    .setDescription("API documentation JWT")
-    .setVersion("1.0.0")
+    .setTitle('TicketLive API')
+    .setDescription('API documentation JWT')
+    .setVersion('1.0.0')
     .addBearerAuth(
       {
-        type: "http",
-        scheme: "bearer",
-        bearerFormat: "JWT",
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
       },
-      "jwt-auth"
+      'jwt-auth',
     )
     .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
